376512b7f6046f4130e57a16f60e2b8dbc27eff3
Dastan <nurgazyuuludastan@gmail.com>
2024-05-23 22:27:54 +0600

    add skeleton for news and banner

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,19 +1,21 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getNews } from '../../api/apiNews'
 import NewBanner from '../../components/newBanner/NewBanner'
-import styles from './Main.module.scss'
-import { useState } from 'react'
-import NewList from '../../components/newsList/NewsList'
 import NewsList from '../../components/newsList/NewsList'
+import Skeleton from '../../components/skeleton/Skeleton'
+import styles from './Main.module.scss'
 
 export const Main = () => {
 	const [news, setNews] = useState(null)
+	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
 		const fetchNews = async () => {
 			try {
+				setIsLoading(true)
 				const response = await getNews()
 				setNews(response.news)
+				setIsLoading(false)
 			} catch (error) {
 				console.log(error)
 			}
@@ -23,8 +25,16 @@ export const Main = () => {
 
 	return (
 		<main className={styles.main}>
-			{news?.length > 0 ? <NewBanner item={news[0]} /> : null}
-			<NewsList news={news} />
+			{news?.length > 0 && !isLoading ? (
+				<NewBanner item={news[0]} />
+			) : (
+				<Skeleton type={'banner'} count={1} />
+			)}
+			{!isLoading ? (
+				<NewsList news={news} />
+			) : (
+				<Skeleton type={'item'} count={10} />
+			)}
 		</main>
 	)
 }
